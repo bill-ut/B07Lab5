e295cfb21160920c12e933d65b825912c03a53d9
Shengtao Liu <lshengtao05@gmail.com>
2025-06-27 21:58:46 -0400

    fix: corrected test1 rectangle coordinates in RectangleTests

--- src/RectangleTests.java
+++ src/RectangleTests.java
@@ -7,9 +7,9 @@ class RectangleTests {
 	@Test
 	void test1() throws RecException {
 		Point A = new Point(0,0);
-		Point B = new Point(0,3);
-		Point C = new Point(3,0);
-		Point D = new Point(3,3);
+		Point B = new Point(3,0);
+		Point C = new Point(3,3);
+		Point D = new Point(0,3);
 		Rectangle rect = new Rectangle(A, B, C, D);
 		assertEquals(12.0, rect.perimeter());
 		assertTrue(rect.isSquare());
@@ -18,8 +18,8 @@ class RectangleTests {
 	void test2() throws RecException{
 		Point A = new Point(0,0);
 		Point B = new Point(4,0);
-		Point C = new Point(0,3);
-		Point D = new Point(4,3);
+		Point C = new Point(4,3);
+		Point D = new Point(0,3);
 		Rectangle rect = new Rectangle(A, B, C, D);
 		assertEquals(14.0, rect.perimeter());
 		assertFalse(rect.isSquare());
